8243fa822f323c95d0e8020f3095e2d8f0cb31c4
Michael <m.berlingieri02@gmail.com>
2021-04-21 15:58:34 +0200

    Iconlayer stazioni

--- app/polveri/src/App.js
+++ app/polveri/src/App.js
@@ -1,36 +1,51 @@
 /// app.js
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import {IconLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
+const coordinates = require('./data/coord.json')
+
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWliZXJsIiwiYSI6ImNrbnI0YnNmMDBsNWcydXF3end5bnJvMnYifQ.-iTvckAzIPYilCLo9G14sA';
 
+const ICON_MAPPING = {
+  marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+};
+
 // Viewport settings
 const INITIAL_VIEW_STATE = {
   longitude: 11.0,
-  latitude: 45.5,
-  zoom: 9,
+  latitude: 45.455,
+  zoom: 9.5,
   pitch: 0,
   bearing: 0
 };
 
-// Data to be used by the LineLayer
-const data = [
-  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
-];
+console.log(coordinates)
 
-function App({data}) {
-  const layers = [
-    new LineLayer({id: 'line-layer', data})
-  ];
+function App ({viewState}) {
+  const layer = new IconLayer({
+    id: 'icon-layer',
+    data: coordinates,
+    pickable: true,
+    // iconAtlas and iconMapping are required
+    // getIcon: return a string
+    iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+    iconMapping: ICON_MAPPING,
+    getIcon: d => 'marker',
+
+    sizeScale: 15,
+    getPosition: d => d.coordinates,
+    getSize: d => 5,
+    getColor: d => [Math.sqrt(d.exits), 140, 0]
+  });
 
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
-      layers={layers}
+      layers={[layer]}
     >
       <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
     </DeckGL>
