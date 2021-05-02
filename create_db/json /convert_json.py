077cdc7d8ada3e4627db9b2d5b1366dd3a6aa811
miberl <m.berlingieri02@gmail.com>
2021-05-02 20:17:32 +0200

    Geojson

--- create_db/json /convert_json.py
+++ create_db/json /convert_json.py
@@ -5,10 +5,15 @@ with open('PM10_centraline_daily_ALL.json') as json_file:
     i = 1
     g_data = {}
     for d in data:
-
-        print ('\"'+ str(i) + '\": '+ d['Legnago']+',')
+        val = 0
+        giarol = int(d['Giarol'])
+        bmilano = int(d['BgoMilano'])
+        if (giarol != -1 and bmilano != -1):
+            val = int((giarol+bmilano)/2)
+        elif giarol !=-1:
+            val = giarol
+        else:
+            val = bmilano
+        
+        print ('\"'+ str(i) + '\": '+ str(val)+',')
         i=i+1
-    
-
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile)
